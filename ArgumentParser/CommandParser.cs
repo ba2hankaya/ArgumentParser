431ec78f69ce2738054f24454b00e46004751a89
ba2hankaya <baa2han.kay@gmail.com>
2025-09-17 17:13:28 +0300

    Add project files.

---
+++ ArgumentParser/CommandParser.cs
@@ -0,0 +1,249 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Dynamic;
+using System.Text.RegularExpressions;
+
+namespace ArgumentParserNS
+{
+    public enum ParserAction
+    {
+        positional,
+        take_value,
+        store_true,
+        store_false
+    }
+    public class ArgumentParser
+    {
+        public ArgumentBuilder AddArgument(params string[] flags)
+        {
+            var arg = new Argument(flags);
+            args.Add(arg);
+            return new ArgumentBuilder(arg);
+        }
+        internal class Argument
+        {
+            public string help = "", dest = "";
+            public List<string> flags;
+            public ParserAction parserAction = ParserAction.take_value;
+            public Type? valueType = null;
+            public Object? value = null;
+            string longFlagPattern = @"(^--)";
+            string shortFlagPattern = @"(^-[^-])";
+
+            public Argument(params string[] flags)
+            {
+                this.flags = flags.ToList();
+                if (flags.Length == 1 && flags[0] != "" && !Regex.IsMatch(flags[0], longFlagPattern) && !Regex.IsMatch(flags[0], shortFlagPattern))
+                {
+                    dest = flags[0];
+                    parserAction = ParserAction.positional;
+                    return;
+                }
+                foreach (string flag in flags)
+                {
+                    if (!Regex.IsMatch(flag, longFlagPattern) && !Regex.IsMatch(flag, shortFlagPattern))
+                    {
+                        throw new ArgumentException($"Flags must start with a dash or double dash.");
+                    }
+                }
+                foreach (string flag in flags)
+                {
+                    if (Regex.IsMatch(flag, longFlagPattern))
+                    {
+                        dest = flag.Remove(0, 2).Replace('-', '_');
+                        return;
+                    }
+                }
+                dest = flags[0].Remove(0, 1).Replace('-', '_');
+            }
+        }
+
+        public class ArgumentBuilder
+        {
+            private Argument _arg;
+
+            internal ArgumentBuilder(Argument arg)
+            {
+                _arg = arg;
+            }
+
+            public ArgumentBuilder WithParserAction(ParserAction action)
+            {
+                _arg.parserAction = action;
+                return this;
+            }
+
+            public ArgumentBuilder WithHelp(string help)
+            {
+                _arg.help = help;
+                return this;
+            }
+
+            public ArgumentBuilder WithType(Type type)
+            {
+                _arg.valueType = type;
+                if (_arg.value != null && _arg.valueType != null && _arg.value.GetType() != _arg.valueType)
+                {
+                    throw new ArgumentException($"Value type and default values don't match.");
+                }
+                return this;
+            }
+
+            public ArgumentBuilder WithDefault(object defaultVal)
+            {
+                _arg.value = defaultVal;
+                if (_arg.value != null && _arg.valueType != null && defaultVal.GetType() != _arg.valueType)
+                {
+                    throw new ArgumentException($"Value type and default values don't match.");
+                }
+                return this;
+            }
+        }
+
+        string prog, desc, epilog;
+        List<Argument> args = new();
+
+        IDictionary<string, object> keyValuePairs = new Dictionary<string, object>();
+        public ArgumentParser(string prog, string desc, string epilog)
+        {
+            this.prog = prog;
+            this.desc = desc;
+            this.epilog = epilog;
+        }
+
+        public ExpandoObject ArgParse(in string[] inputArgs)
+        {
+            try
+            {
+                List<Argument> argscpy = args.ToList();
+                for (int i = 0; i < inputArgs.Length; i++)
+                {
+                    string currentToken = inputArgs[i].Trim();
+                    if (currentToken.StartsWith('-'))
+                    {
+                        Argument? current = argscpy.FirstOrDefault(x => x.flags.Contains(currentToken));
+                        if (current == null)
+                        {
+                            throw new FormatException($"There are no flags corresponding to '{currentToken}'.");
+                        }
+                        switch (current.parserAction)
+                        {
+                            case ParserAction.take_value:
+                                if (i + 1 >= inputArgs.Length)
+                                {
+                                    throw new FormatException($"There isn't a value after value flag '{currentToken}'.");
+                                }
+                                string value = inputArgs[i + 1];
+
+                                if (value == "" || value[0] == '-')
+                                {
+                                    throw new FormatException($"There isn't a value after value flag '{currentToken}'.");
+                                }
+                                if (current.valueType != null)
+                                {
+                                    try
+                                    {
+                                        Convert.ChangeType(value, current.valueType);
+                                    }
+                                    catch
+                                    {
+                                        throw new FormatException($"Value type doesn't match entered value for flag {currentToken}");
+                                    }
+                                }
+                                keyValuePairs.Add(current.dest, value);
+                                i++;
+                                argscpy.Remove(current);
+                                break;
+                            case ParserAction.store_true:
+                                keyValuePairs.Add(current.dest, true);
+                                argscpy.Remove(current);
+                                break;
+                            case ParserAction.store_false:
+                                keyValuePairs.Add(current.dest, false);
+                                argscpy.Remove(current);
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Argument? current = argscpy.FirstOrDefault(x => x.parserAction == ParserAction.positional);
+                        if (current == null)
+                        {
+                            throw new FormatException($"Couldn't parse argument: '{currentToken}'");
+                        }
+                        if (currentToken == "")
+                        {
+                            throw new FormatException($"Couldn't parse arguments, check for whitespaces.");
+                        }
+                        keyValuePairs.Add(current.dest, currentToken);
+                        argscpy.Remove(current);
+                    }
+                }
+
+                foreach (Argument argument in argscpy)
+                {
+                    switch (argument.parserAction)
+                    {
+                        case ParserAction.positional:
+                            throw new FormatException($"The positional argument {argument.flags[0]} was never given.");
+                            break;
+                        case ParserAction.take_value:
+                            if (argument.value == null) //if a default value wasn't provided
+                            {
+                                throw new FormatException($"A value wasn't passed with the {argument.flags[0]} flag.");
+                            }
+                            keyValuePairs.Add(argument.dest, argument.value);
+                            break;
+                        case ParserAction.store_true:
+                            keyValuePairs.Add(argument.dest, false);
+                            break;
+                        case ParserAction.store_false:
+                            keyValuePairs.Add(argument.dest, true);
+                            break;
+                    }
+                }
+                string serialized = JsonConvert.SerializeObject(keyValuePairs);
+                dynamic? expando = JsonConvert.DeserializeObject<ExpandoObject>(serialized, new ExpandoObjectConverter());
+                if ((object?)expando != null)
+                {
+                    return expando;
+                }
+                else
+                {
+                    throw new Exception("No arguments passed to the arg parser.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                throw new Exception("Bad argument.");
+            }
+        }
+
+        public string ConstructHelpMessage()
+        {
+            string usage = $"usage: {prog} ";
+            foreach (Argument arg in args)
+            {
+                switch (arg.parserAction)
+                {
+                    case ParserAction.positional:
+                        usage += $"{arg.flags[0]} ";
+                        break;
+                    case ParserAction.take_value:
+                        usage += $"[{arg.flags[0]} {arg.dest.ToUpper()}] ";
+                        break;
+                    case ParserAction.store_true:
+                        usage += $"[{arg.flags[0]}] ";
+                        break;
+                    case ParserAction.store_false:
+                        usage += $"[{arg.flags[0]}] ";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return usage;
+        }
+    }
+}
---
+++ ArgumentParserTests/ArgumentParserTests.cs
@@ -0,0 +1,201 @@
+﻿using ArgumentParserNS;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ArgumentParserTests
+{
+    [TestClass]
+    public sealed class ArgumentParserTests
+    {
+        [TestMethod]
+        public void EmptyInputTest()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-f");
+            dynamic expando = argparse.ArgParse([""]);
+        }
+
+        [TestMethod]
+        public void TakeValueFlagSingle()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-a");
+            dynamic expando = argparse.ArgParse(["-a", "312"]);
+
+            string expected = "{\"a\":\"312\"}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void TakeValueFlagSingleWithLong()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-f", "--foo");
+            dynamic expando = argparse.ArgParse(["-f", "312"]);
+
+            string expected = "{\"foo\":\"312\"}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void TakeValueFlagMultiple()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-f");
+            argparse.AddArgument("-b");
+            dynamic expando = argparse.ArgParse(["-f", "312", "-b", "stringVal"]);
+
+            string expected = "{\"f\":\"312\",\"b\":\"stringVal\"}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void TakeValueFlagMultipleWithLong()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-f", "--foo");
+            argparse.AddArgument("-b", "--bar");
+            dynamic expando = argparse.ArgParse(["--bar", "312", "-f", "stringVal"]);
+
+            string expected = "{\"bar\":\"312\",\"foo\":\"stringVal\"}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void PositionalFlagSingle()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("foo");
+            dynamic expando = argparse.ArgParse(["input"]);
+
+            string expected = "{\"foo\":\"input\"}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void PositionalFlagMultiple()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("foo");
+            argparse.AddArgument("bar");
+            dynamic expando = argparse.ArgParse(["input", "input2"]);
+
+            string expected = "{\"foo\":\"input\",\"bar\":\"input2\"}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void StoreTrueFalse()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-f", "--foo")
+                .WithParserAction(ParserAction.store_true);
+            argparse.AddArgument("-b", "--bar")
+                .WithParserAction(ParserAction.store_false);
+            dynamic expando = argparse.ArgParse(["-b", "--foo"]);
+
+            string expected = "{\"foo\":true,\"bar\":false}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void StoreTrueFalse2()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-f", "--foo")
+                .WithParserAction(ParserAction.store_true);
+            argparse.AddArgument("-b", "--bar")
+                .WithParserAction(ParserAction.store_false);
+            dynamic expando = argparse.ArgParse(["-b"]);
+
+            string expected = "{\"foo\":false,\"bar\":false}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+        
+        [TestMethod]
+        public void Mixed() 
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-v", "--verbose")
+                .WithParserAction(ParserAction.store_true);
+            argparse.AddArgument("--make-false")
+                .WithParserAction(ParserAction.store_false);
+            argparse.AddArgument("ipaddr");
+            argparse.AddArgument("-p", "--port");
+
+            dynamic expando = argparse.ArgParse("127.0.0.1 -p 4444 -v --make-false".Split());
+
+
+            string expected = "{\"ipaddr\":\"127.0.0.1\",\"port\":\"4444\",\"verbose\":true,\"make_false\":false}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+
+        [TestMethod]
+        public void Mixed2()
+        {
+            ArgumentParser argparse = new ArgumentParser("p", "d", "e");
+            argparse.AddArgument("-v", "--verbose")
+                .WithParserAction(ParserAction.store_true);
+            argparse.AddArgument("--make-false")
+                .WithParserAction(ParserAction.store_false);
+            argparse.AddArgument("ipaddr");
+            argparse.AddArgument("-p", "--port")
+                .WithDefault("4444");
+
+            dynamic expando = argparse.ArgParse("127.0.0.1 -v --make-false".Split());
+
+            string expected = "{\"ipaddr\":\"127.0.0.1\",\"port\":\"4444\",\"verbose\":true,\"make_false\":false}";
+            JObject expectedJson = JObject.Parse(expected);
+
+            string received = JsonConvert.SerializeObject(expando);
+            JObject receivedJson = JObject.Parse(received);
+
+            Assert.IsTrue(JToken.DeepEquals(expectedJson, receivedJson));
+        }
+    }
+}
